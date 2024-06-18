c2c611c6b26f8b4c8422e26338e438f57e7ccdf3
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-06-17 22:49:32 -0300

    report

--- src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,19 @@
 package com.devsuperior.dsmeta.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDate;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+    @Query("SELECT new com.devsuperior.dsmeta.dto.SaleReportDTO(s.id,s.date,s.amount,s.seller.name) " +
+    "FROM Sale s WHERE s.date BETWEEN :minDate AND :maxDate "+
+    "and UPPER(s.seller.name) LIKE UPPER(CONCAT('%', :sellerName, '%'))")
+    Page<SaleReportDTO> searchSaleByPeriodAndSellerName(LocalDate minDate, LocalDate maxDate, String sellerName, Pageable pageable);
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,11 +1,17 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
@@ -19,5 +25,25 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleReportDTO> searchSaleByPeriodAndSellerName(String strMinDate, String strMaxDate, String sellerName, Pageable pageable) {
+		LocalDate maxDate = getMaxDateFromString(strMaxDate);
+		LocalDate minDate = getMinDateFromStringAndMaxDate(strMinDate, maxDate);
+		return repository.searchSaleByPeriodAndSellerName(minDate, maxDate, sellerName, pageable);
+	}
+
+	private LocalDate getMaxDateFromString(String strMaxDate) {
+		if(strMaxDate.isBlank()) {
+			return LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		}
+		return LocalDate.parse(strMaxDate);
+	}
+
+	private LocalDate getMinDateFromStringAndMaxDate(String strMinDate, LocalDate maxDate) {
+		if(strMinDate.isBlank()) {
+			return maxDate.minusYears(1L);
+		}
+		return LocalDate.parse(strMinDate);
 	}
 }
